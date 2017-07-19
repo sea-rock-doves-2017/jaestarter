d02ec524768fb0ede7c6f22c2168cc2e950b6085
Jae Easley <jaesley@gmail.com>
2017-07-19 10:57:51 -0700

    Add validations + tests for Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,3 @@
 class Project < ApplicationRecord
+  validates :title, :description, :amount_donated, :goal_amount, :end_date, presence: true
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#validations' do
+    it { is_expected.to validate_presence_of :title, :description }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :amount_donated }
+    it { is_expected.to validate_presence_of :goal_amount }
+    it { is_expected.to validate_presence_of :end_date }
+  end
 end
